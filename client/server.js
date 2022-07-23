8d5b02646f1e77298c9e38f2e593c9ecb5d8d722
Lewinskie <wangaralewis@gmail.com>
2022-07-23 10:12:02 +0300

    added server.js to use in production mode

---
+++ client/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+const config = require('dotenv');
+
+config.config();
+const configValues = process.env;
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/*', function(req, res) {
+    console.log(`🚀 Client ready at http://localhost:${configValues.PORT || 8082}`);
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(configValues.PORT || 8082);
