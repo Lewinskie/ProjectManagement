93fd41bb69e759e8494776eeb50d5ca5edf2ed81
Lewinskie <wangaralewis@gmail.com>
2022-07-23 12:55:59 +0300

    added changes to index.js for client

--- client/src/index.js
+++ client/src/index.js
@@ -27,7 +27,7 @@ const cache = new InMemoryCache({
 
 // Create the Apollo client
 const client = new ApolloClient({
-  uri: "http://localhost:5000/graphql",
+  uri: process.env.REACT_APP_SERVER_URL,
   cache: cache,
 });
 
